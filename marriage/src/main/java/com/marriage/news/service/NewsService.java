0f24c60498888c12195c085f78125a9be68dfedc
LNLILI <1299901260@qq.com>
2019-03-24 13:46:59 +0800

    first commit

---
+++ marriage/src/main/java/com/marriage/news/service/NewsService.java
@@ -0,0 +1,26 @@
+package com.marriage.news.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.marriage.user.entity.ManInformation;
+
+public interface NewsService {
+	
+	Integer addNews(Map<String, Object> map);
+
+	List<ManInformation> selectNewsBypage(Map<String, Object> map);
+	
+	Integer selectNewsByPageTotal(Map<String, Object> map);
+
+	Integer updateNewsByNewsId(Map<String, Object> map);
+
+	Integer deleteNewsById(Map<String, Object> map);
+
+	List<ManInformation> selectNewsTopIndex(Map<String, Object> map);
+
+	List<ManInformation> selectNewsById(Map<String, Object> map);
+
+	
+
+}
---
+++ marriage/src/main/java/com/marriage/user/dao/UserDao.java
@@ -0,0 +1,36 @@
+package com.marriage.user.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.marriage.user.entity.ManInformation;
+import com.marriage.user.entity.User;
+
+public interface UserDao {
+
+	Integer addUser(Map<String, Object> map);
+
+	Integer addManInformation(Map<String, Object> map);
+
+	List<ManInformation> selectManInformationByPage(Map<String, Object> map);
+	
+	Integer selectManInformationByTotal(Map<String, Object> map);
+	
+	Integer updateManInformationByUserId(Map<String, Object> map);
+
+	Integer updateUserDelFlagByIds(Map<String, Object> map);
+
+	List<User> selectUserByUserName(String userName);
+
+	List<User> userLogin(Map<String, Object> map);
+
+	Integer userAdds(Map<String, Object> map);
+	//添加到user表
+	Integer userAddsUser(Map<String, Object> map);
+
+	User userUserId(Map<String, Object> map);
+
+	List<User> logindown(Map<String, Object> map);
+
+
+}
